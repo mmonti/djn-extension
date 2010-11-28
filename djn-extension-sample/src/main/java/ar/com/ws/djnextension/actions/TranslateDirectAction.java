875fea948c06831e6037c60347083a56d0fe921c
monti.mauro <monti.mauro@d88b5d4d-5c8a-e4ac-499b-66e7943e3be4>
2010-11-28 05:33:51 +0000

    Less verbose mode to annotate a DirectAction. We can use @SpringDirectAction instead of use @Component & @DirectAction

--- djn-extension-sample/src/main/java/ar/com/ws/djnextension/actions/TranslateDirectAction.java
+++ djn-extension-sample/src/main/java/ar/com/ws/djnextension/actions/TranslateDirectAction.java
@@ -7,21 +7,19 @@ import java.util.Map;
 
 import org.apache.commons.fileupload.FileItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
+import ar.com.ws.djn.extension.annotation.SpringDirectAction;
 import ar.com.ws.djnextension.response.Response;
 import ar.com.ws.djnextension.services.TranslatorService;
 
 import com.google.api.translate.Language;
-import com.softwarementors.extjs.djn.config.annotations.DirectAction;
 import com.softwarementors.extjs.djn.config.annotations.DirectFormPostMethod;
 
 /**
  * @author Mauro Monti
  *
  */
-@Component
-@DirectAction
+@SpringDirectAction
 public class TranslateDirectAction {
 
 	/**
